72adf8a6f7a24d979cbcf319780d84368aafb03d
ffirg <phil.griffiths@gmail.com>
2014-12-02 22:48:20 +0000

    Ruby equivalent of the Docker clean shell script

---
+++ docker_clean_all.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+#
+# clean up docker containers and images as they can get out of hand, pretty quickly.
+#
+
+# Decision time
+def if_yes(component)
+
+  print "Delete #{component}? [y/n]: "
+  ans = gets.chomp
+
+  if ans == "y"
+    cleanup(component)
+  end
+
+end
+
+def cleanup(component)
+
+  case component
+
+  when "containers"
+    puts "Removing #{component}:"
+    #containers = `docker ps -a -q`
+    containers = %x[docker ps -a -q].strip.gsub(/\s+/, ' ')
+    containers_cmd = "docker rm #{containers}"
+    system("#{containers_cmd}")
+
+  when "images"
+    puts "Removing #{component}:"
+    images = %x[docker images -q].strip.gsub(/\s+/, ' ')
+    images_cmd = "docker rmi #{images}"
+    system("#{images_cmd}")
+
+  end
+
+end
+
+# STUFF STARTS HAPPENING HERE...
+puts "This script will clean up ALL Docker containers AND/OR images."
+if_yes ("containers")
+if_yes ("images")
+
+# THE END.
